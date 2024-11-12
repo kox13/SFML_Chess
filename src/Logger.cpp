0ec4baf4a8402cb0b58ece8fbfe5f6832322485f
Dawid Wołoszyn <woloszyndawid99@gmail.com>
2024-11-12 18:52:40 +0100

    Logger macros and logs colors

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,6 +1,7 @@
 #include "Logger.h"
 #include <SFML/System.hpp>
 #include <iostream>
+#include <cassert>
 
 static Logger* s_Instance = nullptr;
 
@@ -25,14 +26,33 @@ void Logger::Shutdown() {
     delete s_Instance;
 }
 
+void Logger::LogError(const std::string& error, const char* file, unsigned int line) {
+    assert(file);
+    std::cerr << "\033[" << (int)Color::RED << "m";
+    std::cerr << "ERROR: " << error << " in " << file << " at " << line << "\n";
+    std::cerr << "\033[0m";
+}
+
+void Logger::LogWarning(const std::string& warning, const char* file, unsigned int line) {
+    assert(file);
+    std::cerr << "\033[" << (int)Color::YELLOW << "m";
+    std::cerr << "WARNING: " << warning << " in " << file << " at " << line << "\n";
+    std::cerr << "\033[0m";
+}
+
+void Logger::LogMessage(const std::string& message, Color color) {
+    std::cout << "\033[" << (int)color << "m";
+    std::cout << "MESSAGE: " << message << "\n";
+    std::cout << "\033[0m";
+}
+
 int Logger::overflow(int c) {
     if (c != EOF) {
-        std::cerr << "\033[32m";
+        std::cerr << "\033[47m"; // Only SFML errors have a background to differentiate them from normal errors
+        std::cerr << "\033[" << (int)Color::RED << "m";
         std::cerr << (char)c;
         std::cerr << "\033[0m";
-
-        return c;
     }
 
-    return EOF;
+    return c;
 }
--- src/Logger.h
+++ src/Logger.h
@@ -1,16 +1,38 @@
 #pragma once
 #include <streambuf>
+#include <string>
+
+#define LOG_ERROR(error) Logger::Instance().LogError(error, __FILE__, __LINE__)
+#define LOG_WARNING(warning) Logger::Instance().LogWarning(warning, __FILE__, __LINE__)
+#define LOG_MESSAGE(message) Logger::Instance().LogMessage(message)
+#define LOG_CMESSAGE(message, color) Logger::Instance().LogMessage(message, color)
 
 class Logger : public std::streambuf {
 public:
-	Logger();
-	~Logger() = default;
+	enum class Color {
+		BLACK = 30,
+		RED,
+		GREEN,
+		YELLOW,
+		BLUE,
+		MAGENTA,
+		CYAN,
+		WHITE
+	};
 
 	static Logger& Instance();
 
 	static bool Initialize();
 	static void Shutdown();
 
+	void LogError(const std::string&, const char* file, unsigned int line);
+	void LogWarning(const std::string& warning, const char* file, unsigned int line);
+	void LogMessage(const std::string& message, Color color = Color::WHITE);
+
 	virtual int overflow(int c) override;
+
+private:
+	Logger();
+	~Logger() = default;
 };
 
