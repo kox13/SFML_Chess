0b12aabe730ad967881fea40fbdffef99516c2e1
Dawid Wołoszyn <woloszyndawid99@gmail.com>
2024-11-16 19:39:55 +0100

    AddTexture and AddFont methods

--- src/AssetsManager.cpp
+++ src/AssetsManager.cpp
@@ -1,4 +1,34 @@
 #include "AssetsManager.h"
+#include "Logger.h"
 
-void AssetsManager::AddTexture(unsigned int id, const std::string& path) {}
-void AssetsManager::SetFont(const std::string& path) {}
+void AssetsManager::AddTexture(unsigned int id, const std::string& path) {
+	if (m_Textures.find(id) != m_Textures.end()) {
+		LOG_WARNING("Texture with id %i, already exists", id);
+		return;
+	}
+
+	// TODO: Check if the path is correct and if it has a supported SFML extension, 
+	// if not log/show error and stop the program
+
+	std::shared_ptr<sf::Texture> texture = std::make_shared<sf::Texture>();
+
+	if (texture->loadFromFile(path))
+		m_Textures[id] = texture;
+	else
+		LOG_ERROR("Texture from file %s was not loaded correctly", path);
+}
+
+void AssetsManager::SetFont(const std::string& path) {
+	if (m_Font) {
+		LOG_WARNING("Font is already set");
+		return;
+	}
+
+	// TODO: Check if the path is correct and if it has a supported SFML extension, 
+	// if not log/show error, if m_Font is set do nothing more, if not try to load the default font, else stop the program
+
+	m_Font = std::make_shared<sf::Font>();
+
+	if(!m_Font->loadFromFile(path))
+		LOG_ERROR("Font from file %s was not loaded correctly", path);
+}
--- src/AssetsManager.h
+++ src/AssetsManager.h
@@ -6,6 +6,7 @@
 #include <unordered_map>
 #include <memory>
 
+// TODO: Multiple fonts?????
 class AssetsManager {
 public:
 	AssetsManager() = default;
