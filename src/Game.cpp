97c0aa2892e22cc170b0202a4edb9377b3bdb9a4
Dawid Wołoszyn <woloszyndawid99@gmail.com>
2024-11-16 15:27:59 +0100

    Rewritten Game class and Context struct

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,49 +1,39 @@
 #include "Game.h"
-#include "AssetsManager.h"
-#include "Logger.h"
-	
-Game::Game() 
-    : m_Run(true)
-{
-    if (!InitializeSystems())
-        m_Run = false;
-}
 
-Game::~Game() {
-    ShutdownSystems();
-}
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+Game::Game()
+	: m_Run(true)
+{}
 
 void Game::Run() {
-    // TODO: Log error
-    if (!m_Run)
-        return;
+    sf::RenderWindow& window = *m_Context.m_Window;
+    window.create(sf::VideoMode(1337, 766), "SFML works!");
 
-    sf::RenderWindow window(sf::VideoMode(1366, 768), "SFML works!");
-    sf::Texture texture;
-    texture.loadFromFile("./assets/textures/b_king.png");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
+    float deltaTime;
+    sf::Clock clock;
+    clock.restart();
 
-    while (window.isOpen() && m_Run) {
+    while (window.isOpen()) {
+        deltaTime = clock.restart().asSeconds();
+
+        m_Context.m_GameState->ChangeState();
+        m_Context.m_GameState->ProcessInput();
+        m_Context.m_GameState->Update(deltaTime);
+        m_Context.m_GameState->Draw();
+
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear(sf::Color::White);
-        window.draw(sprite);
+        window.clear();
+        window.draw(shape);
         window.display();
     }
-}
-
-bool Game::InitializeSystems() {
-	return AssetsManager::Initialize() && 
-           Logger::Initialize();
-}
-
-void Game::ShutdownSystems() {
-	AssetsManager::Shutdown();
-    Logger::Shutdown();
 }
--- src/Game.h
+++ src/Game.h
@@ -1,17 +1,41 @@
 #pragma once
 
+#include "AssetsManager.h"
+#include "StateManager.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <memory>
+
+struct Context {
+	AssetsManager& GetAssetsManager() const { return *m_AssetsManager; }
+	StateManager& GetGameState() const { return *m_GameState; }
+	sf::RenderWindow& GetWindow() const { return *m_Window; }
+
+private:
+	Context()
+		: m_AssetsManager(std::make_unique<AssetsManager>()),
+		m_GameState(std::make_unique<StateManager>(*this)),
+		m_Window(std::make_unique<sf::RenderWindow>())
+	{}
+	~Context() = default;
+
+	Context& operator=(Context& other) = delete;
+	Context& operator=(const Context&& other) = delete;
+
+	std::unique_ptr<AssetsManager> m_AssetsManager;
+	std::unique_ptr<StateManager> m_GameState;
+	std::unique_ptr<sf::RenderWindow> m_Window;
+
+	friend class Game;
+};
+
 class Game {
 public:
 	Game();
-	~Game();
+	~Game() = default;
 
 	void Run();
-
-private:
-	bool InitializeSystems();
-	void ShutdownSystems();
-
 private:
 	bool m_Run;
+	Context m_Context;
 };
 
